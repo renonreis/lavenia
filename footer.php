fdf7ef3ed1374fc5430f9510d040d6c5294b3832
aurudin <renon@renon.com.br>
2021-12-07 22:16:18 -0300

    feat: footer layout

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		<section class="newsletter">
 			<div class="container">
 				<div class="row">
-					<div class="col d-flex justify-content-center">
+					<div class="col d-flex justify-content-center align-items-center">
 						<h2>Inscreva-se na nossa <span>newsletter</span></h2>
 						<?php echo do_shortcode('[contact-form-7 id="7" title="Newsletter"]'); ?>
 					</div>
@@ -25,37 +25,37 @@
 		<section class="footer">
 			<div class="container">
 				<div class="row">
-					<div class="col-12 justify-content-center information">
-						<ul class="d-flex">
+					<div class="col-12 information">
+						<ul class="d-flex justify-content-center">
 							<li>
 								<a href="">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/whatsapp.svg" alt=""/>
+									<span><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/whatsapp.svg" alt=""/></span>
 									(12) 3456-7890
 								</a>
 							</li>
 							<li>
 								<a href="">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/phone.svg" alt=""/>
+									<span><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/phone.svg" alt=""/></span>
 									(12) 3456-7890
 								</a>
 							</li>
 							<li>
 								<a href="">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/mail.svg" alt=""/>
+									<span><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/mail.svg" alt=""/></span>
 									laveniacosmeticos@gmail.com
 								</a>
 							</li>
 							<li>
-								<a href="">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/pin.svg" alt=""/>
+								<a href="" class="text-uppercase">
+									<span><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/pin.svg" alt=""/></span>
 									RUA vicente machado,1645 apto 17 , centro, cep 85812-150 - Cascavel pr
 								</a>
 							</li>
 						</ul>
 					</div>
 					<div class="col-12 text-center">
-						<p>Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna. Curabitur blandit tempus porttitor.</p>
-						<p>© All Rights Reserved 2021 - LaVenia.</p>
+						<p class="mb-0">Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna. Curabitur blandit tempus porttitor.</p>
+						<p class="mb-0">© All Rights Reserved 2021 - LaVenia.</p>
 					</div>
 				</div>
 			</div>
